d9eba92c7675f5d704d0c51e0d626f17277d87b5
Axel Smidt <axelpilot78@gmail.com>
2020-05-15 17:06:04 +0200

    Updated chapter 3 exercise.

--- src/com/axelsmidt/javarefresher/chapter3/Account.java
+++ src/com/axelsmidt/javarefresher/chapter3/Account.java
@@ -4,6 +4,10 @@ public class Account {
     private String name;
     private double balance;
 
+    public Account(String name) {
+        this.name = name;
+    }
+
     public void setName(String name) {
         this.name = name;
     }
--- src/com/axelsmidt/javarefresher/chapter3/AccountTest.java
+++ src/com/axelsmidt/javarefresher/chapter3/AccountTest.java
@@ -4,16 +4,11 @@ import java.util.Scanner;
 
 public class AccountTest {
     public static void main(String[] args) {
-        Account myAccount = new Account();
-
-        System.out.printf("Initial name is: %s%n%n", myAccount.getName());
-
         Scanner input = new Scanner(System.in);
         System.out.println("Please enter your name:");
         String theName = input.nextLine();
-        myAccount.setName(theName);
+        Account myAccount = new Account(theName);
 
-        System.out.println();
-        System.out.printf("Name in object myAccount is:%n%s%n", myAccount.getName());
+        System.out.printf("The name of the account holder is: %s%n%n", myAccount.getName());
     }
 } // end class AccountTest
