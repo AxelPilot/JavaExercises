c0673b4ceb6cd2eff7bcc55e1fb0f620383db685
Axel Smidt <axelpilot78@gmail.com>
2020-06-28 15:35:47 +0200

    New commit.

--- src/com/axelsmidt/javarefresher/chapter5/Shapes.java
+++ src/com/axelsmidt/javarefresher/chapter5/Shapes.java
@@ -19,13 +19,14 @@ public class Shapes extends JPanel {
         for (int i = 0; i < 25; i++) {
             switch (choice) {
                 case 1: // draw rectangles
-                    g.drawRect(10+i*10, 10+i*10, 50+i*10, 50+i*10);
+                    g.drawRect(10 + i * 10, 10 + i * 10, 50 + i * 10, 50 + i * 10);
                     break;
                 case 2: // draw ovals
-                    g.drawOval(10+i*10, 10+i*10, 50+i*10, 50+i*10);
+                    g.drawOval(10 + i * 10, 10 + i * 10, 50 + i * 10, 50 + i * 10);
                     break;
                 case 3: // draw concentric circles
-                    g.drawOval(centerX-(10+i*10), centerY-(10+i*10), (10+i*10)*2, (10+i*10)*2);
+                    g.drawOval(centerX - (10 + i * 10), centerY - (10 + i * 10), (10 + i * 10) * 2,
+                            (10 + i * 10) * 2);
             }
         }
     }
